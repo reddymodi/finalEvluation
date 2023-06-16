8e3670578907917bb6ccff7715c58486b115f555
root <root@ip-172-31-35-242.ap-south-1.compute.internal>
2023-06-16 19:02:28 +0000

    first commit

---
+++ terra/main.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_vpc" "myVpc1" {
+  cidr_block = "10.0.0.0/24"
+}
+data "aws_availability_zones" "available_zones" {}
+resource "aws_subnet" "publicSubnet1" {
+  vpc_id            = aws_vpc.myVpc1.id
+  cidr_block        = "10.0.0.0/25"
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+  tags = {
+    Name = "publicSubnet1"
+  }
+}
+resource "aws_subnet" "privateSubnet1" {
+  vpc_id            = aws_vpc.myVpc1.id
+  cidr_block        = "10.0.0.128/25"
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+  tags = {
+    Name = "privateSubnet1"
+  }
+}
+
+resource "aws_internet_gateway" "myIGW1" {
+  vpc_id = aws_vpc.myVpc1.id
+  tags = {
+    Name = "myIGW1"
+  }
+}
+resource "aws_route_table" "myPublicRoute" {
+  vpc_id = aws_vpc.myVpc1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myIGW1.id
+  }
+  tags = {
+    Name = "myRoute"
+  }
+}
+// associate subnet with route table
+resource "aws_route_table_association" "myPublicRouteAssociate" {
+  subnet_id      = aws_subnet.publicSubnet1.id
+  route_table_id = aws_route_table.myPublicRoute.id
+}
+
+
+resource "aws_security_group" "mySecureGrp" {
+  name   = "mySecureGrp"
+  vpc_id = aws_vpc.myVpc1.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    //ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "mySecureGrp"
+  }
+}
+resource "aws_instance" "myEc2Public" {
+  ami                    = "ami-08e5424edfe926b43"
+  instance_type          = "t2.micro"
+  key_name               = "modi1"
+  subnet_id              = aws_subnet.publicSubnet1.id
+  vpc_security_group_ids = [aws_security_group.mySecureGrp.id]
+  associate_public_ip_address = true
+  user_data              = <<-EOF
+              #! /bin/bash
+              echo "hello world!" > hello.txt
+              sudo apt-get update -y
+              sudo apt  install docker.io -y
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
+              aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/y4e4l6z4
+              sudo docker push public.ecr.aws/y4e4l6z4/mkreddy:my
+              sudo docker run -d -p 8080:80 public.ecr.aws/y4e4l6z4/mkreddy:my
+              EOF
+
+  tags = {
+    Name = "tf-ubuntu"
+  }
+}
+
