38d18954eb4457d39e7e4b048dca9123871e5984
reddymodi <114898950+reddymodi@users.noreply.github.com>
2023-06-19 11:33:30 +0530

    Update variable.tf

--- terra/variable.tf
+++ terra/variable.tf
@@ -1,8 +1,1 @@
-variable "access_key" {
-    type = string
-    default = "AKIAXOXXYHGQMBO5SL2X"
-}
-variable "secret_key" {
-    type = string
-    default = "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"
-}
+
